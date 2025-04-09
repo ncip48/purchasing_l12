b07afa1ab28746b9dc60417d6ed18a7126dea988
herlyplanviu <herly@planviu.com>
2025-04-09 16:45:41 +0700

    aa

--- api/api.py
+++ api/api.py
@@ -137,4 +137,4 @@ app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
