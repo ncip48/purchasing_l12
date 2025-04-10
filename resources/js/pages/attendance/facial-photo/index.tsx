113e0b0c33814680854e9966449323457827ec66
herlyplanviu <herly@planviu.com>
2025-04-10 10:53:24 +0700

    validation photo

--- resources/js/pages/attendance/facial-photo/index.tsx
+++ resources/js/pages/attendance/facial-photo/index.tsx
@@ -40,14 +40,16 @@ function Page() {
                                 Add Photo
                             </Button>
                         </div>
-                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                            <Card className="gap-0 overflow-hidden rounded-2xl p-0 shadow-md">
-                                <img src={`/img/${face.photo || ''}`} alt={`Registered Face`} className="h-60 w-full object-cover" />
-                                <CardContent className="p-4 text-center">
-                                    <p className="text-muted-foreground text-sm font-medium">Registered Face</p>
-                                </CardContent>
-                            </Card>
-                        </div>
+                        {face && (
+                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                                <Card className="gap-0 overflow-hidden rounded-2xl p-0 shadow-md">
+                                    <img src={`/img/${face.photo || ''}`} alt={`Registered Face`} className="h-60 w-full object-cover" />
+                                    <CardContent className="p-4 text-center">
+                                        <p className="text-muted-foreground text-sm font-medium">Registered Face</p>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        )}
                         {/* <AttendanceTable
                             user="Herly Chahya Putra"
                             month="April"
