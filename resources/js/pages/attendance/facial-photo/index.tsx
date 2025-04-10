0f9b21a5b25cb3a7588385c3681513e52b09a4e7
herlyplanviu <herly@planviu.com>
2025-04-10 10:50:45 +0700

    hide table time

--- resources/js/pages/attendance/facial-photo/index.tsx
+++ resources/js/pages/attendance/facial-photo/index.tsx
@@ -1,11 +1,9 @@
-import AttendanceTable from '@/components/card/attendance-table';
 import Container from '@/components/container';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem, PageProps } from '@/types';
 import { FaceType } from '@/types/face';
-import { generateMonthlyAttendanceData } from '@/utils/date';
 import { Head, usePage } from '@inertiajs/react';
 import { PlusIcon } from 'lucide-react';
 
@@ -20,12 +18,11 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const attendanceData = {
-    '2025-04-01': '08:30:00',
-    '2025-04-02': '07:15:20',
-    '2025-04-05': '09:00:00',
-    // ... more
-};
+// const attendanceData = {
+//     '2025-04-01': '08:30:00',
+//     '2025-04-02': '07:15:20',
+//     '2025-04-05': '09:00:00',
+// };
 
 function Page() {
     const { face } = usePage<PageProps<{ face: FaceType }>>().props;
@@ -51,12 +48,12 @@ function Page() {
                                 </CardContent>
                             </Card>
                         </div>
-                        <AttendanceTable
+                        {/* <AttendanceTable
                             user="Herly Chahya Putra"
                             month="April"
                             weeks={generateMonthlyAttendanceData(2025, 4, attendanceData)}
                             grandTotal="14h"
-                        />
+                        /> */}
                     </CardContent>
                 </Card>
             </Container>
